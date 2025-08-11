55a1fa71613927090458656527968d961e044fde
Ben Makhlouf <makhlobe@gmail.com>
2025-08-11 09:08:37 -0700

    added a project, started a script for metadata

---
+++ Code/MetadataOrganizing.R
@@ -0,0 +1,6 @@
+################### 
+###### This script is for data organizing and tracking, will produce metadata file photoMetadata.csv 
+
+### List out the three directories 
+
+KuskoDIR <- here("Raw_Images/Kusko") 
