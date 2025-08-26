5a528ec6ae60ac7ed209856a2fd3ad10b9bf06f3
Ben Makhlouf <makhlobe@gmail.com>
2025-08-26 08:56:27 -0700

    progress got names out

--- Code/ShapeOutline.R
+++ Code/ShapeOutline.R
@@ -6,3 +6,16 @@ shape <- shapeR("/Users/benjaminmakhlouf/Research_repos/Integrated_OtoShapeAnaly
 # Detect outlines
 outlinesonly <- detect.outline(shape, threshold = .4, write.outline.w.org = FALSE)
 
+
+# Get the successful outline names 
+names(outlinesonly@outline.list)
+outline_names <- list()
+for(folder in names(outlinesonly@outline.list)) {
+  outline_names[[folder]] <- names(outlinesonly@outline.list[[folder]])
+}
+outline_names
+
+# Now, read in the original csv 
+original_data <- read.csv("/Users/benjaminmakhlouf/Research_repos/Integrated_OtoShapeAnalysis/Data/ForOutlines/FISH.csv")
+
+# remove the .jpg from 
